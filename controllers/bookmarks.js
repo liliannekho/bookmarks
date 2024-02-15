4dbb453a965cf6f500ccc464c25bde9491163fd3
unknown <liliannekho@gmail.com>
2024-02-15 14:44:13 -0500

    created bookmarks controllers

---
+++ controllers/bookmarks.js
@@ -0,0 +1,40 @@
+const Bookmark = require('../models/bookmark')
+
+//delete bookmark
+//create bookmark
+//update bookmark
+
+const destroyBookmark = async (req, res, next) => {
+    try {
+        const deletedBookmark = await Bookmark.findByIdAndDelete(req.params.id)
+        res.locals.data.bookmark = deletedBookmark
+        next()
+    } catch (error) {
+        res.status(400).json({ msg: error.message})
+    }
+}
+
+const updateBookmark = async (req, res, next) => {
+    try {
+        const updatedBookmark = await Bookmark.findByIdAndUpdate(req.params.id)
+        res.locals.data.bookmark = updatedBookmark
+        next()
+    } catch (error) {
+        res.status(400).json({ msg: error.message})
+    }
+}
+
+const createBookmark = async (req, res, next) => {
+    try {
+        const createdBookmark = await Bookmark.create(req.body)
+        res.locals.data.bookmark = createdBookmark
+    } catch (error) {
+        res.status(400).json({ msg: error.message})
+    }
+} 
+
+module.exports = {
+    destroyBookmark,
+    updateBookmark,
+    createBookmark
+}
